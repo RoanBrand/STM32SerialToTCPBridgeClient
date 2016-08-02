fa0a93bd3920fbc8b2f7fa504bf8cd417f95950d
RoanBrand <roan.brand@imqs.co.za>
2016-08-02 19:26:42 +0200

    remove esp8266 specific stuff

--- Inc/PubSubClient.h
+++ Inc/PubSubClient.h
@@ -74,12 +74,7 @@
 #define MQTTQOS1        (1 << 1)
 #define MQTTQOS2        (2 << 1)
 
-#ifdef ESP8266
-#include <functional>
-#define MQTT_CALLBACK_SIGNATURE std::function<void(char*, uint8_t*, unsigned int)> callback
-#else
 #define MQTT_CALLBACK_SIGNATURE void (*callback)(char*, uint8_t*, unsigned int)
-#endif
 
 typedef struct PubSubClient_t {
 	Client* _client;
