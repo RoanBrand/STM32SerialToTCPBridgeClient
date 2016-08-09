0ddd293ee4d93723b75dba7cc53d3b2f309bf20f
RoanBrand <roan.brand@imqs.co.za>
2016-08-09 15:01:54 +0200

    set preemption priorities for ISRs

--- Src/usart.c
+++ Src/usart.c
@@ -89,7 +89,7 @@ void HAL_UART_MspInit(UART_HandleTypeDef* uartHandle)
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
     /* Peripheral interrupt init */
-    HAL_NVIC_SetPriority(USART2_IRQn, 0, 0);
+    HAL_NVIC_SetPriority(USART2_IRQn, 1, 0);
     HAL_NVIC_EnableIRQ(USART2_IRQn);
   /* USER CODE BEGIN USART2_MspInit 1 */
 
